cb73f0baa8b5e41d8cffbb6aa0b197cc37fe4b52
Aidan Honan <19714467+pinqy@users.noreply.github.com>
2023-08-02 01:04:12 -0400

    created db structures

---
+++ dbmanager/architecture.go
@@ -0,0 +1,23 @@
+package dbmanager
+
+import "time"
+
+type Expense struct {
+	ID         int64
+	CategoryID int64
+	Amount     float32
+	Date       time.Time
+	Location   string
+	Notes      string
+}
+
+type Category struct {
+	ID          int64
+	Name        string
+	Description string
+}
+
+type Tag struct {
+	ID   int64
+	Name string
+}
